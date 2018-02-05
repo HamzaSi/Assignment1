3da7a0bb4720e90f4165fb96313c68605b1d5e70
sshankar97 <33465414+sshankar97@users.noreply.github.com>
2018-02-05 10:46:36 -0500

    Update starterbot.py

--- starterbot.py
+++ starterbot.py
@@ -3,22 +3,14 @@ import time
 import re
 from slackclient import SlackClient
 
-
-# instantiate Slack client
 slack_client = SlackClient('xoxb-309708628482-hLqfVfeSnA2LNVeFZkNB97WG')
-# starterbot's user ID in Slack: value is assigned after the bot starts up
 starterbot_id = None
 
-# constants
-RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+RTM_READ_DELAY = 1 
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
 def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
+    
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
             user_id, message = parse_direct_mention(event["text"])
@@ -27,28 +19,18 @@ def parse_bot_commands(slack_events):
     return None, None
 
 def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
+    
     matches = re.search(MENTION_REGEX, message_text)
-    # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
-    """
-    # Default response is help text for the user
-    default_response = "Not sure what you mean." 
-
-    # Finds and executes the given command, filling in response
+  
+    default_response = "Try again." 
     response = None
-    # This is where you start to implement more commands!
    
-    response = command
+   
+    response = "Did you say" + command + "?"
 
-    # Sends the response back to the channel
     slack_client.api_call(
         "chat.postMessage",
         channel=channel,
@@ -57,8 +39,7 @@ def handle_command(command, channel):
 
 if __name__ == "__main__":
     if slack_client.rtm_connect(with_team_state=False):
-        print("Starter Bot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
+        print("Starter Bot is running!")
         starterbot_id = slack_client.api_call("auth.test")["user_id"]
         while True:
             command, channel = parse_bot_commands(slack_client.rtm_read())
@@ -66,4 +47,4 @@ if __name__ == "__main__":
                 handle_command(command, channel)
             time.sleep(RTM_READ_DELAY)
     else:
-        print("Connection failed. Exception traceback printed above.")
+        print("Connection failed. Try again.")
